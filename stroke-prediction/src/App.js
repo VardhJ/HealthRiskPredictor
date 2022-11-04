a6ab06580aa5724a3f83660572bb7b01a4b25cc4
Vardhaman Jain <vardhamanjain2401@gmail.com>
2022-11-04 19:06:19 +0530

    Integrated python backend with Flask

--- stroke-prediction/src/App.js
+++ stroke-prediction/src/App.js
@@ -1,9 +1,46 @@
-
+// Importing modules
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+
+  // const [data, setdata] = useState({
+  //   name: "",
+  //   age: 0,
+  //   date: "",
+  //   programming: "",
+  // });
+
+  // useEffect(() => {
+  //   fetch("/data").then((res) =>
+  //     res.json().then((data) => {
+  //       setdata({
+  //         name: data.Name,
+  //         age: data.Age,
+  //         date: data.Date,
+  //         programming: data.programming,
+  //       });
+  //     })
+  //   );
+  // }, []);
+  function sendUserInfo() {
+    let userinfo = document.getElementById('name').value
+    console.log(userinfo)
+    const request = new XMLHttpRequest()
+    request.open('POST', `/ProcessUserinfo/${JSON.stringify(userinfo)}`)
+    request.send();
+  }
   return (
     <div className="App">
-      <h1>Hey</h1>
+      <header className="App-header">
+        <h1>React and flask</h1>
+        {/* <p>{data.name}</p>
+        <p>{data.age}</p>
+        <p>{data.date}</p>
+        <p>{data.programming}</p> */}
+        <input type='text' name="Name" id="name"></input><br></br>
+        <button type="button" onClick={sendUserInfo}>Submit</button>
+      </header>
     </div>
   );
 }
